88b853ad047669adb851375e70aaffdb620da806
Shaurya <Shouriyatayal1234@gmail.com>
2024-04-11 22:43:48 +0530

    Added It

---
+++ assets/js/typewriter_init.js
@@ -0,0 +1,5 @@
+const instance = new Typewriter('#js-typewriter', {
+    strings: ['The most suitable organization for your business', 'Let`s Walk Together', 'You are on the right track Let`s Catch Up!'],
+    autoStart: true,
+    loop: true,
+});
